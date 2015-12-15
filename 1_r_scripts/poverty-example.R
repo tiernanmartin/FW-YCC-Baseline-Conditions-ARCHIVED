f56b2b45d001a8d3f42a5018bf31573348b5b5ba
tiernanmartin <tiernan.martin@gmail.comgit config --listgit config --listgit congif --listgit config --global user.email tiernan.martin@gmail.com>
2015-12-14 16:23:08 -0800

    script for downloading Seattle neighborhood boundaries data

--- 1_r_scripts/poverty-example.R
+++ 1_r_scripts/poverty-example.R
@@ -22,6 +22,25 @@ library(rgeos)
 
 # GET SPATIAL DATA ------------------------------------------------------------------------------
 
+tracts <- tracts(state = "Washington",county = "King") # download spatial data for all King County census tracts
+
+# Check if the Seattle neighborhood boundaries data has already been downloaded,
+# and if it hasn't then download it.
+
+if(!file.exists("./2_inputs/Neighborhoods/StatePlane/Neighborhoods.dbf.")){  
+        
+        url <- "https://data.seattle.gov/download/2mbt-aqqx/application/zip" # save the URL for the neighborhood boundaries
+        
+        temp <- tempfile() # create a temporary file to hold the compressed download
+        
+        download(url, dest = temp, mode="wb") # download the file
+        
+        unzip (temp, exdir = "./2_inputs/") # extract the ESRI geodatabase file to a project folder
+}
+
+# download spatial data for all Seattle neighborhood boundaries
+
+
 # GET DEMOGRAPHIC DATA ----------------------------------------------------------------------------
 
 # MERGE -------------------------------------------------------------------------------------------
