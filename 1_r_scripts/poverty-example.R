cb59b14e94c8337e242e75aa6436d228872b0eb1
tiernanmartin <tiernan.martin@gmail.comgit config --listgit config --listgit congif --listgit config --global user.email tiernan.martin@gmail.com>
2015-12-14 16:03:39 -0800

    set up script headers

---
+++ 1_r_scripts/poverty-example.R
@@ -0,0 +1,32 @@
+## LEAFLET MAP EXPERIMENT
+
+# PROJECT SETTINGS --------------------------------------------------------------------------------
+
+options(scipen=999)
+
+# LOAD PACKAGES -----------------------------------------------------------------------------------
+
+library(rgdal)    # for readOGR and others
+library(sp)       # for spatial objects
+library(leaflet)  # for interactive maps (NOT leafletR here)
+library(dplyr)    # for working with data frames
+library(ggplot2)  # for plotting
+library(tigris)
+library(acs)
+library(stringr) # to pad fips codes
+library(purrr)
+library(magrittr)
+library(downloader)
+library(tmap)
+library(rgeos)
+
+# GET SPATIAL DATA ------------------------------------------------------------------------------
+
+# GET DEMOGRAPHIC DATA ----------------------------------------------------------------------------
+
+# MERGE -------------------------------------------------------------------------------------------
+
+# CREATE LEAFLET MAP ------------------------------------------------------------------------------
+
+# SAVE MAP ----------------------------------------------------------------------------------------
+
